415d0d459b2b6dbe20faad1591ee99c166e76407
ezequiasispc <ezequiassblanco@gmail.com>
2022-11-09 12:31:25 -0300

    Proyeto colaborativo first branch 0.1

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-num = 20
+from bs4 import BeautifulSoup as bs
+import requests
 
-print(num)
+url = "https://www.bolsadecereales.com/"
+
+html= requests.get(url)
+content= html.content
+soup= bs(content, "lxml")
+
+flash= soup.find("table", {"class":"flash active"})
+
+
+print(flash)
+#print(precio) 
